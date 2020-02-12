032b47e359fdb5130d3c301baa718f6fc14f7a8d
yaroslav.matushevych@innovecs.com <yaroslav.matushevych@innovecs.com>
2020-02-12 13:04:12 +0200

    useSelect, useDis on HomePage, fetching data

--- smart-bit-test/src/containers/HomePage/HomePage.tsx
+++ smart-bit-test/src/containers/HomePage/HomePage.tsx
@@ -1,13 +1,31 @@
 // modules
 import React, { useEffect, memo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// components
+import Button from '../../ui-library/components/Button';
+// actions
+import { messagesFetchData } from '../../actions/messagesActions';
 // styles
 import styles from './HomePage.module.css';
 
 const HomePage: React.FC = memo(() => {
 
+  const messages = useSelector((state: any) => state.messages);
+  const hasErrored = useSelector((state: any) => state.hasErrored);
+  const isLoading = useSelector((state: any) => state.isLoading);
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(messagesFetchData('https://next.json-generator.com/api/json/get/VycIKBzf_'));
+  }, []);
+
+  console.log(messages);
+  
   return (
     <main>
-
+        {(hasErrored) ? <p>Sorry! There was an error loading the items</p> :
+          (isLoading) ? <p>Loading…</p> : <p>{messages[0].id}</p>}
     </main>
   )
 });
