20e6c299713eab6ac0b7464d6cf710f91b915e39
yaroslav.matushevych@innovecs.com <yaroslav.matushevych@innovecs.com>
2020-02-13 11:24:54 +0200

    added profile

--- smart-bit-test/src/containers/HomePage/HomePage.tsx
+++ smart-bit-test/src/containers/HomePage/HomePage.tsx
@@ -8,6 +8,8 @@ import Orders from './components/Orders';
 import UserProjectList from './components/UserProjectList';
 import SmallToDoList from './components/SmallToDoList';
 import Transactions from './components/Transactions';
+// img
+import profileImg from './../../assets/Matushevych.png'
 // styles
 import styles from './HomePage.module.css';
 
@@ -16,6 +18,11 @@ const HomePage: React.FC = memo(() => {
   return (
     <main className={styles.main}>
       <aside className={styles.aside}>
+        <div className={styles.profileContainer}>
+          <img className={styles.profileImg} src={profileImg} alt="person"/>
+          <h4 className={styles.name}>Yroslav Matushevych</h4>
+          <p className={styles.profession}>Front-End Developer</p>
+        </div>
         <nav className={styles.navBar}>
           <NavList />
         </nav>
